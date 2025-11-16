1bb84be70470da95fa9d858fc0351d08a1fe0205
sneha_dubey <yourselflove9760@gmail.com>
2025-11-16 19:59:58 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,1 +1,14 @@
+# api/index.py
+
+from flask import Flask, jsonify
+from mangum import Mangum  # adapter to make Flask serverless
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return jsonify({"message": "Hello from Flask on Vercel!"})
+
+# This wraps your Flask app so Vercel can invoke it as a function
+handler = Mangum(app)
 
